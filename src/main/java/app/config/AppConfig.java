ac5a8cd38467c1adf4fad069d6a741d30fe3b788
Bolotov Alexander <distalker1@gmail.com>
2022-01-18 08:47:25 +0300

    pom update

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,15 @@
 package app.config;
 
+import app.model.AnimalsCage;
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
+@Qualifier("animalsCage")
 public class AppConfig {
 }
